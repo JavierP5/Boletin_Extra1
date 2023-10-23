fa2262689e79a71d28e4e2a8d85dc60af24caeeb
dam1 <dam1@Aulainformatica32>
2023-10-23 09:10:03 +0200

    Terminado boletin

---
+++ src/boletinextra1/Boletin_Extra1.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package boletinextra1;
+
+/**
+ *
+ * @author dam1
+ */
+public class BoletinExtra1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Extra 1
+        System.out.println("Javier Portela García\n26 febrero 2005");
+        // Extra 2
+        System.out.println("Programación\n1ºParcial=""+10+"\n2ºParcial="+10+\n Nota Final="+10+");"
+        // Extra 3
+        final float precio = 90, descuentoporcentaje= 25;
+        float descuento = precio*descuentoporcentaje/100;
+        
+        System.out.println("Precio final="+(precio-descuento)+"€");
+        
+    }
+    
+}
